3ad10802ec5d1c3f9f7d3808f6a4a842da83a42f
MiguelADSilva <miguelaozinho9910@gmail.com>
2020-10-09 01:22:35 +0100

    Config project

---
+++ src/Pages/Main/main.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Container, Title } from './style';
+
+export default function App() {
+    return (
+        <Container>
+                <Title>Miguel</Title>
+        </Container>
+    )
+}
---
+++ src/Pages/Main/style.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+/* eslint-disable no-undef */
+import styled from 'styled-components/native';
+
+export const Container = styled.View `
+    align-items: center;
+    padding: 40px 20px;
+`;
+
+export const Title = styled.Text `
+    font-size: 30px;
+    padding: 0 10px;
+`;
